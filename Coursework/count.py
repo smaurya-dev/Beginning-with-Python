ab7cde58c20d19845a74cab0bd934859cc509265
smaurya-dev <satiiyam.code@gmail.com>
2026-09-15 21:54:43 +0530

    Fixed a bug(well, i don't know what it is called but it was not able to take lowercase when the word in sentence was stored in uppercase so i fixed it) in count.py

--- Coursework/count.py
+++ Coursework/count.py
@@ -7,7 +7,12 @@ def count_letter(text, letter):
 
 sentence = input("What's the word or sentence? ")
 target = input("What's the target letter? ")
-if target in sentence: 
-    print("The letter", target, "appears", count_letter(sentence, target), "times")
-else: 
-    print("Sure about the target?")
+
+if len(target) != 1:
+    print("Please enter one letter as the target.")
+else:
+    times = count_letter(sentence, target)
+    if times == 0:
+        print("Sure about the target?")
+    else:
+        print("The letter", target, "appears", times, "times")
