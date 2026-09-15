968caa77f2c6c9553e5632bb3f69475adae134bd
smaurya-dev <satiiyam.code@gmail.com>
2026-09-15 21:43:43 +0530

    a program that calculates mean through give list of five subjects

--- Coursework/mean.py
+++ Coursework/mean.py
@@ -1,3 +1,10 @@
-import statistics
+def average(marks):
+    total = 0
+    count = 0
+    for m in marks:
+        total = total + m
+        count = count + 1
+    return total / count
 
-print(statistics.mean([90, 100]))
+marks = list(input("What's the marks in five subjects? "))
+print("Average marks =", average(marks))
