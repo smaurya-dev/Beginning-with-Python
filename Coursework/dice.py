b846b0eb88685fa6da4bd10b6b30a76d6b48d454
smaurya-dev <satiiyam.code@gmail.com>
2026-09-18 22:53:50 +0530

    A dice game !!

---
+++ Coursework/dice.py
@@ -0,0 +1,13 @@
+import random
+
+def roll_dice():
+    while True:
+        user_choice = input("Press 'Enter' to roll the dice or type 'quit' to exit: ").strip().lower()
+        if user_choice == 'quit':
+            print("Thanks for playing!")
+            break
+        
+        roll = random.randint(1, 6)
+        print(f"You rolled a {roll}!\n")
+
+roll_dice()
