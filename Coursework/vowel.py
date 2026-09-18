7feea4ecc6a93982f191b0c1da2df1112fe53fda
smaurya-dev <satiiyam.code@gmail.com>
2026-09-18 22:48:26 +0530

    Revised the vowel.py

--- Coursework/vowel.py
+++ Coursework/vowel.py
@@ -1,10 +1,11 @@
 def count_vowels(text):
-    vowels = "aeiou"
+    vowels = "aeiouAEIOU"
     count = 0
-    for ch in text.lower():
-        if ch in vowels:
-            count = count + 1
+    for char in text:
+        if char in vowels:
+            count += 1
     return count
 
-word = input("What's the word? ")
-print("Vowels in", word, "=", count_vowels(word))
+user_text = input("Enter a sentence to count its vowels: ")
+total_vowels = count_vowels(user_text)
+print(f"There are {total_vowels} vowels in your sentence.")
